5caa9072eca5a038f19cfda590df183a13e4822c
yumasakai99 <60962936+yumasakai99@users.noreply.github.com>
2020-02-13 00:31:23 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="ja">
+    <head>
+        <title>monmy</title>
+        <link rel="stylesheet" href="style.css">
+        <meta charset="utf-8">
+    </head>
+    
+    <body>
+        <header>
+            <div class="header-wrapper">
+                <div class="header-left">
+                    <img class="logo" src="logo.jpg" alt="monmy">
+                </div>
+                <div class="header-right">
+                    <a>top</a>
+                    <a>items</a>
+                    <a>sns</a>
+                </div>
+            </div>
+        </header>
+        
+        <div class="main">
+            <img src="image1.jpg" alt="image1">
+        </div>
+        
+        <div class="concept">
+            <p>シンプルなデザインで<br>男女問わないアイテムを販売しています。</p>
+        </div>
+        
+        <div class="items">
+            <div class="item">
+                <img src="item1.jpg" alt="item1">
+                <p>フーディー</p>
+            </div>
+            <div class="item">
+                <img src="item1.jpg" alt="item1">
+                <p>フーディー</p>
+            </div>
+            <div class="item">
+                <img src="item1.jpg" alt="item1">
+                <p>フーディー</p>
+            </div>
+        </div>
+        
+         <div class="items">
+            <div class="item">
+                <img src="item1.jpg" alt="item1">
+                <p>フーディー</p>
+            </div>
+            <div class="item">
+                <img src="item1.jpg" alt="item1">
+                <p>フーディー</p>
+            </div>
+            <div class="item">
+                <img src="item1.jpg" alt="item1">
+                <p>フーディー</p>
+            </div>
+        </div>
+        
+        <footer>
+            <p>©️Hitomi Monma,2020.</p>
+        </footer>
+    </body>
+</html>
